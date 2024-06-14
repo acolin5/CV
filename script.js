4018586361dcc56948a3ca7255e09f18c5ebb000
acolin5 <141000966+acolin5@users.noreply.github.com>
2024-06-13 20:46:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*In order to get that typing effect use this layour*/
 document.addEventListener('DOMContentLoaded', function () {
     var options = {
-        strings: ["Student.", "Web Developer.", "Tech Enthusiast.", "Lifelong Learner." ], // Replace with your name
+        strings: ["Student.", "Web Developer.", "Software Developer.", "Tech Enthusiast.", "Lifelong Learner." ], 
         typeSpeed: 50,
         backSpeed: 50,
         loop: true
