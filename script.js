b5216f756faa47fe95335cf23bafd605096de02e
acolin5 <141000966+acolin5@users.noreply.github.com>
2025-03-20 17:27:13 -0700

    Update script.js
    removed some lines

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*In order to get that typing effect use this layour*/
 document.addEventListener('DOMContentLoaded', function () {
     var options = {
-        strings: ["Student.", "Web Developer.", "Software Developer.", "Tech Enthusiast.", "Lifelong Learner." ], 
+        strings: ["Student.", "Developer.", "Tech Enthusiast.", "Lifelong Learner." ], 
         typeSpeed: 50,
         backSpeed: 50,
         loop: true
