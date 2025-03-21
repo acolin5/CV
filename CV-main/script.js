f2e415b55657ac91f24d8ad22ce308895249a57f
acolin5 <141000966+acolin5@users.noreply.github.com>
2025-03-20 17:22:43 -0700

    Add files via upload

---
+++ CV-main/script.js
@@ -0,0 +1,11 @@
+/*In order to get that typing effect use this layour*/
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: ["Student.", "Web Developer.", "Software Developer.", "Tech Enthusiast.", "Lifelong Learner." ], 
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    };
+
+    new Typed('.typed-name', options);
+});
